071d1b6bf1045aa84f0f6aa1e438899e85f09882
Adhun U <adhunvkm7@gmail.com>
2026-07-16 16:06:59 +0530

    Created transaction utility

---
+++ database/transaction_manager.go
@@ -0,0 +1,17 @@
+package database
+
+import "gorm.io/gorm"
+
+type TransactionManager struct {
+	db *gorm.DB
+}
+
+func NewTransactionManager(db *gorm.DB) TransactionManager {
+	return TransactionManager{db: db}
+}
+
+func (t TransactionManager) Run(fn func() error) {
+	t.db.Transaction(func(tx *gorm.DB) error {
+		return fn()
+	})
+}
